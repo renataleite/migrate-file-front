e5e28b159178459261303e7400a904c3462b16f2
RENATA GUEDES <renataleitesg@gmail.com>
2023-04-03 08:01:51 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+interface FormData {
+  file: File | null;
+}
+
+const App = () => {
+  const [formData, setFormData] = useState<FormData>({
+    file: null,
+  });
+
+  const [result, setResult] = useState<String>("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData.file);
+    const formDataToSend = new FormData();
+
+    if (formData.file) {
+      formDataToSend.append("file", formData.file);
+    }
+
+    const response = await fetch("https://localhost:7212/RegisterFile", {
+      method: "POST",
+      body: formDataToSend,
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      setResult(result);
+      console.log(result);
+    } else {
+      console.error(response.statusText);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files ? event.target.files[0] : null;
+    setFormData({ ...formData, file });
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formFile">
+          <Form.Label>File:</Form.Label>
+          <Form.Control type="file" name="file" onChange={handleFileChange} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <p>{result}</p>
+    </>
+  );
+};
+
+export default App;
